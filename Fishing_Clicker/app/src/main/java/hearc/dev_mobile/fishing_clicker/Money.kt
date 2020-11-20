db31d182c75610caac1700ee6c6808ce33d3f7e1
Monnet Joris <joris.monnet@he-arc.ch>
2020-11-20 02:12:47 +0100

    function value to string

--- Fishing_Clicker/app/src/main/java/hearc/dev_mobile/fishing_clicker/Money.kt
+++ Fishing_Clicker/app/src/main/java/hearc/dev_mobile/fishing_clicker/Money.kt
@@ -3,15 +3,19 @@ package hearc.dev_mobile.fishing_clicker
 import kotlin.math.pow
 
 class Money {
-    private val NUMBER_OF_DIGIT_DISPLAYED=6
+    private val digitDisplayedPLusOne = 7   //power of ten
     private var value : Long = 0
-    private var thousands : Int = 0
+    private var thousands : Int = 0         //3 power of ten by 3
     private val mthousandsList : MutableList<String> = generateThousandsList()
 
-    fun verifyAndShowValue(): String{
-        if(value < 0) throw Exception("Error on money value")
-        val testValue = value/(10.0.pow(thousands))
-        return (if(testValue>10.0.pow(NUMBER_OF_DIGIT_DISPLAYED)) testValue.toString() else value.toString())+mthousandsList[++thousands]
+    fun valueToString(value : Long): String{
+        val stringValue = value.toString()
+        if(value < 999999) return stringValue
+        var power = 1
+        while((stringValue.length-6)-3.0*power>0)power++
+        if((stringValue.length-6)%3==0)
+            return stringValue.substring(0,6) + mthousandsList[power]
+        return stringValue.substring(0,stringValue.length-3*power) + mthousandsList[power]
     }
 
     private fun generateThousandsList() : MutableList<String>{
@@ -24,9 +28,5 @@ class Money {
             for(SecondChar in CharRange('a','z'))
                 thousandsList.add(firstChar.toString() + SecondChar.toString())
         return thousandsList
-    }
-
-    fun payUpgrade(value : Int ){
-
     }
 }
--- Fishing_Clicker/app/src/test/java/hearc/dev_mobile/fishing_clicker/MoneyTEst.kt
+++ Fishing_Clicker/app/src/test/java/hearc/dev_mobile/fishing_clicker/MoneyTEst.kt
@@ -3,14 +3,13 @@ package hearc.dev_mobile.fishing_clicker
 import org.junit.Test
 
 import org.junit.Assert.*
-import kotlin.math.pow
 
 /**
  *
  */
 class MoneyTEst {
     @Test
-    fun generateList() {
+    fun generateThousandsList(){
         val thousandsList : MutableList<String> = mutableListOf("","k","M","B")
         //a to z
         for(element in CharRange('a','z'))
@@ -25,5 +24,32 @@ class MoneyTEst {
         assertEquals(thousandsList[26+4], "aa") //value after one time alphabet with only one char and the 4 first values
         assertEquals(thousandsList[52+5], "bb") //value after one time alphabet with 'a' as first character and the 30 before
         assertEquals(thousandsList[thousandsList.size-1],"zz")
+    }
+    @Test
+    fun testVtStr(){
+        assertEquals(valueToString(123456),"123456")
+        assertEquals(valueToString(1234567),"1234k")
+        assertEquals(valueToString(12345678),"12345k")
+        assertEquals(valueToString(123456789),"123456k")
+        assertEquals(valueToString(1234567891),"1234M")
+
+    }
+
+    private fun valueToString(value : Long): String{
+        val thousandsList : MutableList<String> = mutableListOf("","k","M","B")
+        //a to z
+        for(element in CharRange('a','z'))
+            thousandsList.add(element.toString())
+        //aa to zz
+        for(firstChar in CharRange('a','z'))
+            for(SecondChar in CharRange('a','z'))
+                thousandsList.add(firstChar.toString() + SecondChar.toString())
+        val stringValue = value.toString()
+        if(value < 999999) return stringValue
+        var power = 1
+        while((stringValue.length-6)-3.0*power>0)power++
+        if((stringValue.length-6)%3==0)
+            return stringValue.substring(0,6) + thousandsList[power]
+        return stringValue.substring(0,stringValue.length-3*power) + thousandsList[power]
     }
 }
