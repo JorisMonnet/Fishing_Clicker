0a6f9d4208cee46158bf1ce950447b9eda42ac02
Monnet Joris <joris.monnet@he-arc.ch>
2020-11-21 04:18:20 +0100

    persitance of data done

--- Fishing_Clicker/app/src/main/java/hearc/dev_mobile/fishing_clicker/User.kt
+++ Fishing_Clicker/app/src/main/java/hearc/dev_mobile/fishing_clicker/User.kt
@@ -5,25 +5,29 @@ import java.math.BigInteger
 import java.util.*
 import kotlin.math.pow
 
-class User(sharedPref: SharedPreferences) {
+class User() {
     var level: Int = 0
     private var click = BigInteger.ONE
     var money: Money = Money()
-
-    init {
-        val moneyPref = sharedPref.getString("GeneralMoney", "0")
-        money.value = if(moneyPref!=null) BigInteger(moneyPref) else BigInteger.ZERO
-        val clickPref = sharedPref.getString("Click", "1")
-        money.value = if(clickPref!=null) BigInteger(clickPref) else BigInteger.ONE
-        level = sharedPref.getInt("Level", 0)
-    }
-
 
     fun getClickValue(): BigInteger {
         return click.multiply(BigInteger.valueOf(10.0.pow(level).toLong()))
     }
 
-    fun saveData(){
-        
+    fun createValuesFromPref(sharedPrefUser : SharedPreferences){
+        val moneyPref = sharedPrefUser.getString("GeneralMoney", "0")
+        money.value = if(moneyPref!=null) BigInteger(moneyPref) else BigInteger.ZERO
+        val clickPref = sharedPrefUser.getString("Click", "1")
+        click = if(clickPref!=null) BigInteger(clickPref) else BigInteger.ONE
+        level = sharedPrefUser.getInt("Level", 0)
+    }
+
+    fun saveData(sharedPrefUser : SharedPreferences){
+        with(sharedPrefUser.edit()){
+            putInt("Level",level)
+            putString("Click",click.toString())
+            putString("GeneralMoney",money.value.toString())
+            apply()
+        }
     }
 }
