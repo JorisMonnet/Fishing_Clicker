32ac2d3229b6d1f785eacd60034b7b223bd01985
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-01-25 00:04:11 +0100

    create activity popupshake and edit xml view

---
+++ Fishing_Clicker/app/src/main/java/hearc/dev_mobile/fishing_clicker/PopUpShake.kt
@@ -0,0 +1,11 @@
+package hearc.dev_mobile.fishing_clicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopUpShake : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_shake)
+    }
+}
